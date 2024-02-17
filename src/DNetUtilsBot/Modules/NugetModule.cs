db7d2e88d2b6962d50a8bdbc53222a5e613e3322
Misha133 <mihagribkov133@gmail.com>
2024-02-17 23:30:11 +0300

    a

--- src/DNetUtilsBot/Modules/NugetModule.cs
+++ src/DNetUtilsBot/Modules/NugetModule.cs
@@ -11,19 +11,44 @@ public class NugetModule(ILogger<NugetModule> logger, ILoggerFactory loggerFacto
 
     [SlashCommand("dnet-stats", "Get statistics for DNet nuget packages")]
     public async Task DNetStatsAsync()
-    {
+	{
+		await DeferAsync();
+
         var p = await _repository.GetResourceAsync<PackageSearchResource>();
 
+		var stats = new Dictionary<string, long?>
+		{
+			{ "Discord.Net", null },
+			{ "Discord.Net.Core", null },
+            { "Discord.Net.Rest", null },
+            { "Discord.Net.WebSocket", null },
+            { "Discord.Net.Interactions", null },
+            { "Discord.Net.Commands", null }
+		};
+
         var filter = new SearchFilter(true);
-        var meta = (await p.SearchAsync("Discord.Net", filter, 0, 1, NullLogger.Instance, CancellationToken.None)).First();
-        var core = (await p.SearchAsync("Discord.Net.Core", filter, 0, 1, NullLogger.Instance, CancellationToken.None)).First();
-        var rest = (await p.SearchAsync("Discord.Net.Rest", filter, 0, 1, NullLogger.Instance, CancellationToken.None)).First();
-        var socket = (await p.SearchAsync("Discord.Net.WebSocket", filter, 0, 1, NullLogger.Instance, CancellationToken.None)).First();
-        var interactions = (await p.SearchAsync("Discord.Net.Interactions", filter, 0, 1, NullLogger.Instance, CancellationToken.None)).First();
-        var commands = (await p.SearchAsync("Discord.Net.Commands", filter, 0, 1, NullLogger.Instance, CancellationToken.None)).First();
 
+		foreach (var stat in stats)
+		{
+			var result = await p.SearchAsync(stat.Key, filter, 0, 10, NullLogger.Instance, CancellationToken.None);
 
+            if (result.Any(x => x.Identity.Id == stat.Key))
+			{
+                stats[stat.Key] = result.First(x => x.Identity.Id == stat.Key).DownloadCount;
+			}
+		}
 
+		var emb = new EmbedBuilder()
+			.WithColor(0xff00)
+			.WithTitle("`Discord.Net` Nuget Stats")
+			.WithDescription($"Total downloads: `{stats.Sum(x => x.Value ?? 0)}`")
+			.WithCurrentTimestamp();
 
-    }
+		foreach (var stat in stats)
+		{
+			emb.AddField(stat.Key, $"`{stat.Value.ToString() ?? "Unknown"}`", true);
+		}
+
+		await FollowupAsync(embed: emb.Build());
+	}
 }
